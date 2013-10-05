6437bef4726b80c0bc53813b19f1a48408af35e5
Constantin Wenger <constantin.wenger@googlemail.com>
2013-10-06 01:33:51 +0200

    Added new Heroes

--- Dota2Statistics/src/general/Hero.java
+++ Dota2Statistics/src/general/Hero.java
@@ -1,11 +1,108 @@
 package general;
+
 public enum Hero {
-	ALL_HEROES(-1, "npc_dota_all_heroes", "All Heroes"), ANTI_MAGE(1, "npc_dota_hero_antimage", "Anti-Mage"), AXE(2, "npc_dota_hero_axe", "Axe"), BANE(3, "npc_dota_hero_bane", "Bane"), BLOODSEEKER(4, "npc_dota_hero_bloodseeker", "Bloodseeker"), CRYSTAL_MAIDEN(5, "npc_dota_hero_crystal_maiden", "Crystal Maiden"), DROW_RANGER(6, "npc_dota_hero_drow_ranger", "Drow Ranger"), EARTHSHAKER(7, "npc_dota_hero_earthshaker", "Earthshaker"), JUGGERNAUT(8, "npc_dota_hero_juggernaut", "Juggernaut"), MIRANA(9, "npc_dota_hero_mirana", "Mirana"), MORPHLING(10, "npc_dota_hero_morphling", "Morphling"), SHADOW_FIEND(11, "npc_dota_hero_nevermore", "Shadow Fiend"), PHANTOM_LANCER(12, "npc_dota_hero_phantom_lancer", "Phantom Lancer"), PUCK(13, "npc_dota_hero_puck", "Puck"), PUDGE(14, "npc_dota_hero_pudge", "Pudge"), RAZOR(15, "npc_dota_hero_razor", "Razor"), SAND_KING(16, "npc_dota_hero_sand_king", "Sand King"), STORM_SPIRIT(17, "npc_dota_hero_storm_spirit", "Storm Spirit"), SVEN(18, "npc_dota_hero_sven", "Sven"), TINY(19, "npc_dota_hero_tiny", "Tiny"), VENGEFUL_SPIRIT(20, "npc_dota_hero_vengefulspirit", "Vengeful Spirit"), WINDRUNNER(21, "npc_dota_hero_windrunner", "Windrunner"), ZEUS(22, "npc_dota_hero_zuus", "Zeus"), KUNKKA(23, "npc_dota_hero_kunkka", "Kunkka"), HERO_WITH_I_23LINA(25, "npc_dota_hero_lina", "Lina"), LION(26, "npc_dota_hero_lion", "Lion"), SHADOW_SHAMAN(27, "npc_dota_hero_shadow_shaman", "Shadow Shaman"), SLARDAR(28, "npc_dota_hero_slardar", "Slardar"), TIDEHUNTER(29, "npc_dota_hero_tidehunter", "Tidehunter"), WITCH_DOCTOR(30, "npc_dota_hero_witch_doctor", "Witch Doctor"), LICH(31, "npc_dota_hero_lich", "Lich"), RIKI(32, "npc_dota_hero_riki", "Riki"), ENIGMA(33, "npc_dota_hero_enigma", "Enigma"), TINKER(34, "npc_dota_hero_tinker", "Tinker"), SNIPER(35, "npc_dota_hero_sniper", "Sniper"), NECROLYTE(36, "npc_dota_hero_necrolyte", "Necrolyte"), WARLOCK(37, "npc_dota_hero_warlock", "Warlock"), BEASTMASTER(38, "npc_dota_hero_beastmaster", "Beastmaster"), QUEEN_OF_PAIN(39, "npc_dota_hero_queenofpain", "Queen of Pain"), VENOMANCER(40, "npc_dota_hero_venomancer", "Venomancer"), FACELESS_VOID(41, "npc_dota_hero_faceless_void", "Faceless Void"), SKELETON_KING(42, "npc_dota_hero_skeleton_king", "Skeleton King"), DEATH_PROPHET(43, "npc_dota_hero_death_prophet", "Death Prophet"), PHANTOM_ASSASSIN(44, "npc_dota_hero_phantom_assassin", "Phantom Assassin"), PUGNA(45, "npc_dota_hero_pugna", "Pugna"), TEMPLAR_ASSASSIN(46, "npc_dota_hero_templar_assassin", "Templar Assassin"), VIPER(47, "npc_dota_hero_viper", "Viper"), LUNA(48, "npc_dota_hero_luna", "Luna"), DRAGON_KNIGHT(49, "npc_dota_hero_dragon_knight", "Dragon Knight"), DAZZLE(50, "npc_dota_hero_dazzle", "Dazzle"), CLOCKWERK(51, "npc_dota_hero_rattletrap", "Clockwerk"), LESHRAC(52, "npc_dota_hero_leshrac", "Leshrac"), NATURE_S_PROPHET(53, "npc_dota_hero_furion", "Nature's Prophet"), LIFESTEALER(54, "npc_dota_hero_life_stealer", "Lifestealer"), DARK_SEER(55, "npc_dota_hero_dark_seer", "Dark Seer"), CLINKZ(56, "npc_dota_hero_clinkz", "Clinkz"), OMNIKNIGHT(57, "npc_dota_hero_omniknight", "Omniknight"), ENCHANTRESS(58, "npc_dota_hero_enchantress", "Enchantress"), HUSKAR(59, "npc_dota_hero_huskar", "Huskar"), NIGHT_STALKER(60, "npc_dota_hero_night_stalker", "Night Stalker"), BROODMOTHER(61, "npc_dota_hero_broodmother", "Broodmother"), BOUNTY_HUNTER(62, "npc_dota_hero_bounty_hunter", "Bounty Hunter"), WEAVER(63, "npc_dota_hero_weaver", "Weaver"), JAKIRO(64, "npc_dota_hero_jakiro", "Jakiro"), BATRIDER(65, "npc_dota_hero_batrider", "Batrider"), CHEN(66, "npc_dota_hero_chen", "Chen"), SPECTRE(67, "npc_dota_hero_spectre", "Spectre"), ANCIENT_APPARITION(68, "npc_dota_hero_ancient_apparition", "Ancient Apparition"), DOOM(69, "npc_dota_hero_doom_bringer", "Doom"), URSA(70, "npc_dota_hero_ursa", "Ursa"), SPIRIT_BREAKER(71, "npc_dota_hero_spirit_breaker", "Spirit Breaker"), GYROCOPTER(72, "npc_dota_hero_gyrocopter", "Gyrocopter"), ALCHEMIST(73, "npc_dota_hero_alchemist", "Alchemist"), INVOKER(74, "npc_dota_hero_invoker", "Invoker"), SILENCER(75, "npc_dota_hero_silencer", "Silencer"), OUTWORLD_DEVOURER(76, "npc_dota_hero_obsidian_destroyer", "Outworld Devourer"), LYCANTHROPE(77, "npc_dota_hero_lycan", "Lycanthrope"), BREWMASTER(78, "npc_dota_hero_brewmaster", "Brewmaster"), SHADOW_DEMON(79, "npc_dota_hero_shadow_demon", "Shadow Demon"), LONE_DRUID(80, "npc_dota_hero_lone_druid", "Lone Druid"), CHAOS_KNIGHT(81, "npc_dota_hero_chaos_knight", "Chaos Knight"), MEEPO(82, "npc_dota_hero_meepo", "Meepo"), TREANT_PROTECTOR(83, "npc_dota_hero_treant", "Treant Protector"), OGRE_MAGI(84, "npc_dota_hero_ogre_magi", "Ogre Magi"), UNDYING(85, "npc_dota_hero_undying", "Undying"), RUBICK(86, "npc_dota_hero_rubick", "Rubick"), DISRUPTOR(87, "npc_dota_hero_disruptor", "Disruptor"), NYX_ASSASSIN(88, "npc_dota_hero_nyx_assassin", "Nyx Assassin"), NAGA_SIREN(89, "npc_dota_hero_naga_siren", "Naga Siren"), KEEPER_OF_THE_LIGHT(90, "npc_dota_hero_keeper_of_the_light", "Keeper of the Light"), WISP(91, "npc_dota_hero_wisp", "Wisp"), VISAGE(92, "npc_dota_hero_visage", "Visage"), SLARK(93, "npc_dota_hero_slark", "Slark"), MEDUSA(94, "npc_dota_hero_medusa", "Medusa"), TROLL_WARLORD(95, "npc_dota_hero_troll_warlord", "Troll Warlord"), CENTAUR_WARRUNNER(96, "npc_dota_hero_centaur", "Centaur Warrunner"), MAGNUS(97, "npc_dota_hero_magnataur", "Magnus"), TIMBERSAW(98, "npc_dota_hero_shredder", "Timbersaw"), BRISTLEBOG(99, "npc_dota_hero_bristleback", "Bristlebog"), TUSK(100, "npc_dota_hero_tusk", "Tusk"), SKYWRATH_MAGE(101, "npc_dota_hero_skywrath_mage", "Skywrath Mage");
+	ALL_HEROES(-1, "npc_dota_all_heroes", "All Heroes"), ANTI_MAGE(1,
+			"npc_dota_hero_antimage", "Anti-Mage"), AXE(2, "npc_dota_hero_axe",
+			"Axe"), BANE(3, "npc_dota_hero_bane", "Bane"), BLOODSEEKER(4,
+			"npc_dota_hero_bloodseeker", "Bloodseeker"), CRYSTAL_MAIDEN(5,
+			"npc_dota_hero_crystal_maiden", "Crystal Maiden"), DROW_RANGER(6,
+			"npc_dota_hero_drow_ranger", "Drow Ranger"), EARTHSHAKER(7,
+			"npc_dota_hero_earthshaker", "Earthshaker"), JUGGERNAUT(8,
+			"npc_dota_hero_juggernaut", "Juggernaut"), MIRANA(9,
+			"npc_dota_hero_mirana", "Mirana"), MORPHLING(10,
+			"npc_dota_hero_morphling", "Morphling"), SHADOW_FIEND(11,
+			"npc_dota_hero_nevermore", "Shadow Fiend"), PHANTOM_LANCER(12,
+			"npc_dota_hero_phantom_lancer", "Phantom Lancer"), PUCK(13,
+			"npc_dota_hero_puck", "Puck"), PUDGE(14, "npc_dota_hero_pudge",
+			"Pudge"), RAZOR(15, "npc_dota_hero_razor", "Razor"), SAND_KING(16,
+			"npc_dota_hero_sand_king", "Sand King"), STORM_SPIRIT(17,
+			"npc_dota_hero_storm_spirit", "Storm Spirit"), SVEN(18,
+			"npc_dota_hero_sven", "Sven"), TINY(19, "npc_dota_hero_tiny",
+			"Tiny"), VENGEFUL_SPIRIT(20, "npc_dota_hero_vengefulspirit",
+			"Vengeful Spirit"), WINDRUNNER(21, "npc_dota_hero_windrunner",
+			"Windrunner"), ZEUS(22, "npc_dota_hero_zuus", "Zeus"), KUNKKA(23,
+			"npc_dota_hero_kunkka", "Kunkka"), HERO_WITH_I_23LINA(25,
+			"npc_dota_hero_lina", "Lina"), LION(26, "npc_dota_hero_lion",
+			"Lion"), SHADOW_SHAMAN(27, "npc_dota_hero_shadow_shaman",
+			"Shadow Shaman"), SLARDAR(28, "npc_dota_hero_slardar", "Slardar"), TIDEHUNTER(
+			29, "npc_dota_hero_tidehunter", "Tidehunter"), WITCH_DOCTOR(30,
+			"npc_dota_hero_witch_doctor", "Witch Doctor"), LICH(31,
+			"npc_dota_hero_lich", "Lich"), RIKI(32, "npc_dota_hero_riki",
+			"Riki"), ENIGMA(33, "npc_dota_hero_enigma", "Enigma"), TINKER(34,
+			"npc_dota_hero_tinker", "Tinker"), SNIPER(35,
+			"npc_dota_hero_sniper", "Sniper"), NECROLYTE(36,
+			"npc_dota_hero_necrolyte", "Necrolyte"), WARLOCK(37,
+			"npc_dota_hero_warlock", "Warlock"), BEASTMASTER(38,
+			"npc_dota_hero_beastmaster", "Beastmaster"), QUEEN_OF_PAIN(39,
+			"npc_dota_hero_queenofpain", "Queen of Pain"), VENOMANCER(40,
+			"npc_dota_hero_venomancer", "Venomancer"), FACELESS_VOID(41,
+			"npc_dota_hero_faceless_void", "Faceless Void"), SKELETON_KING(42,
+			"npc_dota_hero_skeleton_king", "Skeleton King"), DEATH_PROPHET(43,
+			"npc_dota_hero_death_prophet", "Death Prophet"), PHANTOM_ASSASSIN(
+			44, "npc_dota_hero_phantom_assassin", "Phantom Assassin"), PUGNA(
+			45, "npc_dota_hero_pugna", "Pugna"), TEMPLAR_ASSASSIN(46,
+			"npc_dota_hero_templar_assassin", "Templar Assassin"), VIPER(47,
+			"npc_dota_hero_viper", "Viper"), LUNA(48, "npc_dota_hero_luna",
+			"Luna"), DRAGON_KNIGHT(49, "npc_dota_hero_dragon_knight",
+			"Dragon Knight"), DAZZLE(50, "npc_dota_hero_dazzle", "Dazzle"), CLOCKWERK(
+			51, "npc_dota_hero_rattletrap", "Clockwerk"), LESHRAC(52,
+			"npc_dota_hero_leshrac", "Leshrac"), NATURE_S_PROPHET(53,
+			"npc_dota_hero_furion", "Nature's Prophet"), LIFESTEALER(54,
+			"npc_dota_hero_life_stealer", "Lifestealer"), DARK_SEER(55,
+			"npc_dota_hero_dark_seer", "Dark Seer"), CLINKZ(56,
+			"npc_dota_hero_clinkz", "Clinkz"), OMNIKNIGHT(57,
+			"npc_dota_hero_omniknight", "Omniknight"), ENCHANTRESS(58,
+			"npc_dota_hero_enchantress", "Enchantress"), HUSKAR(59,
+			"npc_dota_hero_huskar", "Huskar"), NIGHT_STALKER(60,
+			"npc_dota_hero_night_stalker", "Night Stalker"), BROODMOTHER(61,
+			"npc_dota_hero_broodmother", "Broodmother"), BOUNTY_HUNTER(62,
+			"npc_dota_hero_bounty_hunter", "Bounty Hunter"), WEAVER(63,
+			"npc_dota_hero_weaver", "Weaver"), JAKIRO(64,
+			"npc_dota_hero_jakiro", "Jakiro"), BATRIDER(65,
+			"npc_dota_hero_batrider", "Batrider"), CHEN(66,
+			"npc_dota_hero_chen", "Chen"), SPECTRE(67, "npc_dota_hero_spectre",
+			"Spectre"), ANCIENT_APPARITION(68,
+			"npc_dota_hero_ancient_apparition", "Ancient Apparition"), DOOM(69,
+			"npc_dota_hero_doom_bringer", "Doom"), URSA(70,
+			"npc_dota_hero_ursa", "Ursa"), SPIRIT_BREAKER(71,
+			"npc_dota_hero_spirit_breaker", "Spirit Breaker"), GYROCOPTER(72,
+			"npc_dota_hero_gyrocopter", "Gyrocopter"), ALCHEMIST(73,
+			"npc_dota_hero_alchemist", "Alchemist"), INVOKER(74,
+			"npc_dota_hero_invoker", "Invoker"), SILENCER(75,
+			"npc_dota_hero_silencer", "Silencer"), OUTWORLD_DEVOURER(76,
+			"npc_dota_hero_obsidian_destroyer", "Outworld Devourer"), LYCANTHROPE(
+			77, "npc_dota_hero_lycan", "Lycanthrope"), BREWMASTER(78,
+			"npc_dota_hero_brewmaster", "Brewmaster"), SHADOW_DEMON(79,
+			"npc_dota_hero_shadow_demon", "Shadow Demon"), LONE_DRUID(80,
+			"npc_dota_hero_lone_druid", "Lone Druid"), CHAOS_KNIGHT(81,
+			"npc_dota_hero_chaos_knight", "Chaos Knight"), MEEPO(82,
+			"npc_dota_hero_meepo", "Meepo"), TREANT_PROTECTOR(83,
+			"npc_dota_hero_treant", "Treant Protector"), OGRE_MAGI(84,
+			"npc_dota_hero_ogre_magi", "Ogre Magi"), UNDYING(85,
+			"npc_dota_hero_undying", "Undying"), RUBICK(86,
+			"npc_dota_hero_rubick", "Rubick"), DISRUPTOR(87,
+			"npc_dota_hero_disruptor", "Disruptor"), NYX_ASSASSIN(88,
+			"npc_dota_hero_nyx_assassin", "Nyx Assassin"), NAGA_SIREN(89,
+			"npc_dota_hero_naga_siren", "Naga Siren"), KEEPER_OF_THE_LIGHT(90,
+			"npc_dota_hero_keeper_of_the_light", "Keeper of the Light"), WISP(
+			91, "npc_dota_hero_wisp", "Wisp"), VISAGE(92,
+			"npc_dota_hero_visage", "Visage"), SLARK(93, "npc_dota_hero_slark",
+			"Slark"), MEDUSA(94, "npc_dota_hero_medusa", "Medusa"), TROLL_WARLORD(
+			95, "npc_dota_hero_troll_warlord", "Troll Warlord"), CENTAUR_WARRUNNER(
+			96, "npc_dota_hero_centaur", "Centaur Warrunner"), MAGNUS(97,
+			"npc_dota_hero_magnataur", "Magnus"), TIMBERSAW(98,
+			"npc_dota_hero_shredder", "Timbersaw"), BRISTLEBOG(99,
+			"npc_dota_hero_bristleback", "Bristlebog"), TUSK(100,
+			"npc_dota_hero_tusk", "Tusk"), SKYWRATH_MAGE(101,
+			"npc_dota_hero_skywrath_mage", "Skywrath Mage"), ABADDON(102,
+			"npc_dota_hero_abaddon", "Abaddon"), ELDERTITAN(103,
+			"npc_dota_hero_elder_titan", "Elder Titan"), LEGIONCOMMANDER(104,
+			"npc_dota_hero_legion_commander", "Legion Commander");
 
 	private final int id;
 	private final String name, localName;
-	
-	Hero (int id, String name, String locName) {
+
+	Hero(int id, String name, String locName) {
 		this.id = id;
 		this.name = name;
 		this.localName = locName;
@@ -14,19 +111,19 @@ public enum Hero {
 	public final int getId() {
 		return this.id;
 	}
-	
+
 	public final String toString() {
 		return this.localName;
 	}
-	
+
 	public final String getName() {
 		return this.name;
 	}
-	
+
 	public final String getLocalName() {
 		return localName;
 	}
-	
+
 	public final static Hero getHeroById(int id) {
 		for (Hero h : Hero.values()) {
 			if (h.id == id) {
